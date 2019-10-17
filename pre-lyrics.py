b5617e356238ebc9353473391d8a484e8d740efa
Unknown <gvm104@gmail.com>
2019-10-17 13:10:07 +0200

    changes because of the new get_songs structure
    TO DO: fix Genius requests.

---
+++ pre-lyrics.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+import genius_
+from termcolor import colored
+
+
+
+PATH = os.path.dirname(os.path.abspath(__file__))+'/'
+
+path_2_allsongs = PATH+'/songs_data/all_songs.tsv'
+
+all_songs = pd.read_csv(path_2_allsongs,sep='\t')
+
+song_names = all_songs['Song1'].tolist()
+artist_names = all_songs['Artist1'].tolist()
+
+
+for index, song in enumerate(song_names):
+	if song == 'Name':
+		pass
+	else:
+		artist = artist_names[index]
+		print(colored(' --->\n '+song+' by '+artist,'green'))
+		genius_.main(song,artist)
+
