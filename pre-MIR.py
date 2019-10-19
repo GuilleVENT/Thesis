86204a67352f9f33d5b1f6e0f1887dde127dfdcc
Unknown <gvm104@gmail.com>
2019-10-19 19:41:32 +0200

    https://trello.com/c/Bclwb8mG
    not finished

--- pre-MIR.py
+++ pre-MIR.py
@@ -95,12 +95,90 @@ def download_30s(song_id,song_link):
 	print("		"+colored(song_id+'.mp3','green'))
 
 
+def init2():
+
+	PATH = os.path.dirname(os.path.abspath(__file__))+'/'
+
+	path_2_setlist = PATH+'pl_setlist/'
+
+	PL_data = PATH+'PL_DATA/'
+
+	headers = ['Song_ID','spect_centroid_v' ,'rolloff85_mean', 'rolloff85_std','brightness_mean','brightness_std','spectralflux_mean','spectralflux_std','zerocross_rate','RMS_energy_mean','RMS_energy_std','lowenergy_v','tempo_v','pulseclarity_v','onsets_mean','onsets_std','onsets_peakpos','onsets_peakval','key_v' ,'key_mode_v' , 'ASR_v', 'mfcc_mean' ,'mfcc_std' ,'pitch_mean', 'pitch_std','event_dens_v', 'roughness_mean','roughness_std','cepstrum_mean','cepstrum_std','chroma_mean','chroma_std', 'key_st_mean','key_st_std','key_mode_v','ton_centroid_mean','ton_centroid_std']
+
+
+	if not os.path.exists(PL_data):
+		os.makedirs(PL_data)
+
+	for user in os.listdir(path_2_setlist):
+		path_ = path_2_setlist+user+'/'
+		for pl in os.listdir(path_):
+			file = path_+pl
+			print(file)
+
+
+			###### 
+			#PL_data+user+'/'+pl+'/'
+			if not os.path.exists(PL_data+user+'/'+pl+'/'):
+				os.makedirs(PL_data+user+'/'+pl+'/')
+
+			
+			## check if Audio Features already exist
+			## update
+			output_file = PL_data+user+'/'+pl+'/MIRaudio_features.tsv'
+			try:
+				audio_features_df = pd.read_csv(output_file,sep='\t')
+				print(' Audio Features prior')
+				print(audio_features_df)
+			except FileNotFoundError:
+				### WRITE DataFrame
+				print(colored(' File Not Found'),'red')
+				audio_features_df = pd.DataFrame(columns = headers)
+			######
+		
+
+			setlist = pd.read_csv(file,sep='\t')
+
+			reduced_df = setlist[['Song2','Preview_URL']]
+			print(reduced_df)
+			
+			for index, row in reduced_df.iterrows():
+				song_id 	= row['Song2']
+				song_link	= row['Preview_URL']
+				if song_link.startswith('https://'):
+
+					## download
+					download_30s(song_id,song_link)
+					##
+					name_mp3 = temp_path+song_id+'.mp3'
+					##  MATLAB
+					feat_vector = eng.features_ext_3(name_mp3)
+					##
+					#print(type(feat_vector))
+					feat_ = list(np.array(feat_vector._data))
+					#print(feat_)
+					feat_.insert(0, song_id)
+					
+					print(' -- Audio Features --')
+					print(feat_)
+					print(' --     (as DF)    --')
+
+					df = pd.DataFrame([feat_],columns= headers)
+					
+					print(df)
+					print(' --                --')
+
+					result_df = pd.concat([audio_features_df,df]).drop_duplicates().reset_index(drop=True)
+					print(result_df)
+
+				
+
+					result_df.to_csv(output_file,sep='\t',index=False)
 
 
 
 
 
 
+#init()
 
-
-init()
+init2()
