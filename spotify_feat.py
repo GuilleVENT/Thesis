459235dbe9257579ab2e48c7138be0eaf8a67385
Unknown <gvm104@gmail.com>
2019-10-24 00:54:58 +0200

    bugfix
    NaN exception

--- spotify_feat.py
+++ spotify_feat.py
@@ -19,69 +19,82 @@ def init2():
 	if not os.path.exists(PL_data):
 		os.makedirs(PL_data)
 
+
+
 	for user in os.listdir(path_2_setlist):
-		path_ = path_2_setlist+user+'/'
-		for pl in os.listdir(path_):
-			file = path_+pl
-			print(" Path to Playlist Setlist File")
-			print(file)
-			print(" Playlist:")
-			print(colored(pl[:-4],'cyan'))
-			print("  by user:")
-			print(colored(user,'cyan'))
-			
+		
+		##debugging	
+		#if user == 'chillhopmusic':
 
-			###### 
-			# if path not exist
-			if not os.path.exists(PL_data+user+'/'+pl[:-4]+'/'):
-				os.makedirs(PL_data+user+'/'+pl[:-4]+'/')
+			path_ = path_2_setlist+user+'/'
+			for pl in os.listdir(path_):
+				file = path_+pl
+				print(" Path to Playlist Setlist File")
+				print(file)
+				print(" Playlist:")
+				print(colored(pl[:-4],'cyan'))
+				print("  by user:")
+				print(colored(user,'cyan'))
+				
 
-			# read playlist-setlist
-			setlist = pd.read_csv(file,sep='\t')
-			songs_list = setlist['Song2'].tolist()
-			del songs_list[0]	## = ID 		
+				###### 
+				# if path not exist
+				if not os.path.exists(PL_data+user+'/'+pl[:-4]+'/'):
+					os.makedirs(PL_data+user+'/'+pl[:-4]+'/')
 
-			if len(songs_list) != 0:
+				# read playlist-setlist
+				setlist = pd.read_csv(file,sep='\t')
+				songs_list = setlist['Song2'].tolist()
+				del songs_list[0]	## = ID 		
 
-				for song_id in songs_list:
+				if len(songs_list) != 0:
 
-					## check if Spotify Features already exist
-					## update
-					output_file = PL_data+user+'/'+pl[:-4]+'/Spotify_features.tsv'
-					try:
-						features_df = pd.read_csv(output_file,sep='\t')
-						print(' Spotify Features prior')
-						print(features_df)
-						empty=False
-						if len(features_df['Song_ID'])==0:  ## cannot happen reason: -12 lines
-							empty = True
-					except FileNotFoundError:
-						### WRITE DataFrame
-						print(colored(' File Not Found','red'))
-						features_df = pd.DataFrame(columns = headers)
-						empty=True
-						######
+					for song_id in songs_list:
 
-					if song_id in features_df['Song_ID'].tolist():
-						print(colored(song_id+' \n already in '+ output_file,'green'))
-						pass
-					
+						## check if Spotify Features already exist
+						## update
+						output_file = PL_data+user+'/'+pl[:-4]+'/Spotify_features.tsv'
+						try:
+							features_df = pd.read_csv(output_file,sep='\t')
+							print(' Spotify Features prior')
+							print(features_df)
+							empty=False
+							if len(features_df['Song_ID'])==0:  ## cannot happen reason: -12 lines
+								empty = True
+						except FileNotFoundError:
+							### WRITE DataFrame
+							print(colored(' File Not Found','red'))
+							features_df = pd.DataFrame(columns = headers)
+							empty=True
+							######
 
-					else:
-						print(song_id)
-						features_res = req_spotify_song_features(song_id)
+						if song_id in features_df['Song_ID'].tolist():
+							print(colored(song_id+' \n already in '+ output_file,'green'))
+							pass
+						
+						elif song_id == 'not available':
+							pass
 
-						print(features_res)
-						if empty== True:
-							features_df = features_res
 						else:
-							features_df = pd.concat([features_df,features_res],axis=0).drop_duplicates().reset_index(drop=True)
-						features_df.to_csv(output_file,sep='\t',index=False)
-			else:
-				print('...')#think of something--> these pl cannot be analyzed
+							print(song_id)
+							features_res = req_spotify_song_features(song_id)
+
+							print(features_res)
+							if empty== True:
+								features_df = features_res
+							else:
+								features_df = pd.concat([features_df,features_res],axis=0).drop_duplicates().reset_index(drop=True)
+							features_df.to_csv(output_file,sep='\t',index=False)
+
+					#req_spotify_songLIST_features(songs_list)
+
+				else:
+					print('...')#think of something--> these pl cannot be analyzed
 
 
-
+## def req_spotify_songLIST_features(songs_list):
+##
+## TO DO : LIST request of bellow vvvvv 
 
 def req_spotify_song_features(song_id):
 	
@@ -97,16 +110,23 @@ def req_spotify_song_features(song_id):
 	#print(response.json())
 
 	data = response.json()
-	print(data)
+	#print(data)
 	#print(data['audio_features'])
-	#sys.exit()
+	##sys.exit()
 	#list_of_dics = data['audio_features']
-	if data['audio_features']:
+	
+	if data['audio_features'] == [None]:
 		## 
 		## this is yet to be fixed
-		##
-		sys.exit('crash') 
 
+		print(response.text)
+		##
+		print(song_id)
+
+		row_of_nans = write_NaNs(song_id)
+		return row_of_nans
+	
+	# else
 	dict_ = data['audio_features']
 
 	features_df = order_data(dict_)
@@ -116,6 +136,11 @@ def req_spotify_song_features(song_id):
 	
 	return features_df
 
+def write_NaNs(song_id):
+	dict_ = {'Song_ID': song_id, 'acousticness': 'NaN', 'danceability': 'NaN', 'duration_ms':'NaN', 'energy': 'NaN', 'instrumentalness': 'NaN','key':'NaN', 'liveness': 'NaN','loudness':'NaN', 'mode':'NaN', 'speechiness': 'NaN', 'tempo':'NaN' ,'time_signature':'NaN', 'valence':'NaN' }
+	row_of_nans = pd.DataFrame([dict_])
+	print(row_of_nans)
+	return row_of_nans
 
 def order_data(dict_):																																					## change this vvv 
 	print("... PRIOR:")																																					## VV POS: 1 VVVVV 
